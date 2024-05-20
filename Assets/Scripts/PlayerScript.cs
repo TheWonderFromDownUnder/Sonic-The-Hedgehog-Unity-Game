24ac40131470758adb3108addb3a8f8bf84f6a1c
TheWonderFromDownUnder <52037295+TheWonderFromDownUnder@users.noreply.github.com>
2024-05-20 11:22:55 +0100

    Implemented tile palette and player animations

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("moveX", moveX);
+
+        bool isMoving = !Mathf.Approximately(moveX, 0f);
+
+        animator.SetBool("isMoving", isMoving);
     }
 }
